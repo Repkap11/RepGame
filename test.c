3f1140cf607f7592ed57e24d1fea81ed2c10bde2
Paul Repka <repkap11@gmail.com>
2017-10-08 20:16:48 -0400

    Use game state to change display
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- test.c
+++ test.c
@@ -8,6 +8,7 @@
 #define pr_debug(fmt, ...) \
         do { if (DEBUG) fprintf(stderr, "%s:%d:%s():"fmt"\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__); } while (0)
 
+#define test pr_debug();
 
 typedef struct {
     int exitGame;
@@ -30,32 +31,51 @@ typedef struct {
 
 } RepGameState;
 
-RepGameState gameState;
-
-void initilizeGameState(RepGameState gameState){
-    gameState.exitGame = 0;
-    gameState.colorR = 1;
-    gameState.colorG = 0;
-    gameState.colorB = 0;
+void initilizeGameState(RepGameState* gameState){
+    gameState->exitGame = 0;
+    gameState->colorR = 1;
+    gameState->colorG = 0;
+    gameState->colorB = 0;
 }
+
+void display(RepGameState* gameState) {
+    glClearColor(gameState->colorR, gameState->colorG, gameState->colorB, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+
+}
+
+void gameTick(RepGameState *gameState){
+	if (gameState->exitGame){
+		//Don't bother updating the state if the game is exiting
+		return;
+	}
+	if (gameState->input.arrows.left){
+		gameState->colorR = 1;
+	} else {
+		gameState->colorR = 0;
+	}
+}
+
+RepGameState globalGameState;
 
 void arrowKeyDownInput(int key, int x, int y) {
     switch(key) {
         case GLUT_KEY_UP:
-            gameState.exitGame = 1;
-            gameState.input.arrows.up = 1;
+            globalGameState.exitGame = 1;
+            globalGameState.input.arrows.up = 1;
             pr_debug("Up Arrow 1 %d %d", x, y);
         break;
         case GLUT_KEY_DOWN:
-            gameState.input.arrows.down = 1;
+            globalGameState.input.arrows.down = 1;
             pr_debug("Down Arrow 1 %d %d", x, y);
         break;
         case GLUT_KEY_LEFT:
-            gameState.input.arrows.left = 1;
+            globalGameState.input.arrows.left = 1;
             pr_debug("Left Arrow 1 %d %d", x, y);
         break;
         case GLUT_KEY_RIGHT:
-            gameState.input.arrows.right = 1;
+            globalGameState.input.arrows.right = 1;
             pr_debug("Right Arrow 1 %d %d", x, y);
         break;
     }
@@ -63,20 +83,20 @@ void arrowKeyDownInput(int key, int x, int y) {
 void arrowKeyUpInput(int key, int x, int y) {
     switch(key) {
         case GLUT_KEY_UP:
-            gameState.exitGame = 1;
-            gameState.input.arrows.up = 0;
+            globalGameState.exitGame = 1;
+            globalGameState.input.arrows.up = 0;
             pr_debug("Up Arrow 0 %d %d", x, y);
         break;
         case GLUT_KEY_DOWN:
-            gameState.input.arrows.down = 0;
+            globalGameState.input.arrows.down = 0;
             pr_debug("Down Arrow 0 %d %d", x, y);
         break;
         case GLUT_KEY_LEFT:
-            gameState.input.arrows.left = 0;
+            globalGameState.input.arrows.left = 0;
             pr_debug("Left Arrow 0 %d %d", x, y);
         break;
         case GLUT_KEY_RIGHT:
-            gameState.input.arrows.right = 0;
+            globalGameState.input.arrows.right = 0;
             pr_debug("Right Arrow 0 %d %d", x, y);
         break;
     }
@@ -84,31 +104,19 @@ void arrowKeyUpInput(int key, int x, int y) {
 
 
 void mouseInput(int button, int state, int x, int y){
-
     switch(button) {
         case GLUT_LEFT_BUTTON:
-            gameState.input.mouse.left = !state;
+            globalGameState.input.mouse.left = !state;
             pr_debug("Left Click %d", !state);
         break;
         case GLUT_RIGHT_BUTTON:
-            gameState.input.mouse.right = !state;
+            globalGameState.input.mouse.right = !state;
             pr_debug("Right Click %d", !state);
         break;
         case GLUT_MIDDLE_BUTTON:
-            gameState.input.mouse.middle = !state;
+            globalGameState.input.mouse.middle = !state;
             pr_debug("Middle Click %d", !state);
     }
-}
-
-
-void display(RepGameState gameState) {
-    glClearColor(gameState.colorR, gameState.colorG, gameState.colorB, 0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glFlush();
-
-}
-
-void gameTick(RepGameState gameState){
 }
 
 int main(int argc, char**argv) {
@@ -116,16 +124,14 @@ int main(int argc, char**argv) {
     //glutInitWindowPosition(100,100);
     //glutInitWindowSize(500,500);
     glutCreateWindow("RepGame");
-    //glutDisplayFunc(display);
     glutSpecialFunc(arrowKeyDownInput);
     glutSpecialUpFunc(arrowKeyUpInput);
-
     glutMouseFunc(mouseInput);
-    initilizeGameState(gameState);
-    while (!gameState.exitGame){
+    initilizeGameState(&globalGameState);
+    while (!globalGameState.exitGame){
         glutMainLoopEvent();
-        gameTick(gameState);
-        display(gameState);
+        gameTick(&globalGameState);
+        display(&globalGameState);
     }
     glutLeaveMainLoop();
     return 0;
