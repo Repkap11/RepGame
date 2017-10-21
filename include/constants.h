1e553e0eb8d56b11ed3a7ad45044430a693262f5
Paul Repka <repkap11@gmail.com>
2017-10-21 16:16:55 -0400

    Make an define to use a smaller world
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- include/constants.h
+++ include/constants.h
@@ -1,15 +1,25 @@
 #ifndef HEADER_CONSTANTS_H
 #define HEADER_CONSTANTS_H
 
-#define CHUNK_SIZE 8
+#define LARGE_WORLD
 
+#ifdef LARGE_WORLD
+#define MOVEMENT_SENSITIVITY 400.0f // How sensitive the arrow keys are
 #define CHUNK_RADIUS_X 160 / CHUNK_SIZE
 #define CHUNK_RADIUS_Y 64 / CHUNK_SIZE
 #define CHUNK_RADIUS_Z 160 / CHUNK_SIZE
+#else
+#define MOVEMENT_SENSITIVITY 100.0f // How sensitive the arrow keys are
+#define CHUNK_RADIUS_X 64 / CHUNK_SIZE
+#define CHUNK_RADIUS_Y 64 / CHUNK_SIZE
+#define CHUNK_RADIUS_Z 64 / CHUNK_SIZE
+#endif
+
+#define CHUNK_SIZE 8
+
 #define BLOCK_SCALE 1.0f
 
-#define MOVEMENT_SENSITIVITY 400.0f // How sensitive the arrow keys are
-#define CAMERA_SIZE 0.1f            // Defines how much crop is in front (low for minecraft)
+#define CAMERA_SIZE 0.1f // Defines how much crop is in front (low for minecraft)
 #define PERSON_HEIGHT 40.0f
 #define DRAW_DISTANCE 10000.0f
 #define STARTING_ANGLE_H 35.0f
