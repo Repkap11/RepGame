d359c8c8a07a1cceec1541ec8bb0f098c0d7366b
Paul Repka <repkap11@gmail.com>
2019-12-13 02:14:46 -0500

    Build appimage is stages, which is slower
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- makefiles/appimage.mk
+++ makefiles/appimage.mk
@@ -8,19 +8,30 @@ appimage: $(TARGET)-1-x86_64.AppImage
 
 clean: clean-appimage
 
-out/appimage: | out
-	mkdir -p out/appimage
-
 clean-appimage:
 	rm -rf out/appimage
 	rm -f $(TARGET)-1-x86_64.AppImage
 
-$(TARGET)-1-x86_64.AppImage: out/linux/$(TARGET) src/linux/$(TARGET).desktop bitmaps/icon_512.png linux_build | out/appimage
-	VERSION=1 linux_build/linuxdeploy-x86_64.AppImage\
+out/appimage: | out
+	mkdir -p appimage
+
+out/appimage/AppRun: src/linux/$(TARGET).desktop | out/appimage
+	linux_build/linuxdeploy-x86_64.AppImage\
 		--appdir out/appimage \
-		--executable out/linux/$(TARGET) \
-		--desktop-file src/linux/$(TARGET).desktop \
-		--icon-file bitmaps/icon_512.png \
+		--executable out/linux/$(TARGET)
+
+out/appimage/$(TARGET).desktop: out/linux/$(TARGET) out/appimage/AppRun src/linux/$(TARGET).desktop linux_build | out/appimage
+	linux_build/linuxdeploy-x86_64.AppImage\
+		--appdir out/appimage \
+		--desktop-file src/linux/$(TARGET).desktop
+
+out/appimage/usr/share/icons/hicolor/512x512/apps/icon_512.png: out/linux/$(TARGET) bitmaps/icon_512.png linux_build | out/appimage
+	linux_build/linuxdeploy-x86_64.AppImage\
+		--appdir out/appimage \
+		--icon-file bitmaps/icon_512.png
+
+$(TARGET)-1-x86_64.AppImage: out/appimage/AppRun out/appimage/$(TARGET).desktop out/appimage/usr/share/icons/hicolor/512x512/apps/icon_512.png linux_build | out/appimage
+	VERSION=1 linux_build/linuxdeploy-x86_64.AppImage\
 		--appdir out/appimage --output appimage
 
 .PHONY: appimage clean-appimage appimage-run
