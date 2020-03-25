cdf565729fdfd8f346aba9423de464d6dbbdd78b
Paul Repka <repkap11@gmail.com>
2020-03-25 01:21:25 -0400

    Fix bugs building on other platforms
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- src/windows/multiplayer.cpp
+++ src/windows/multiplayer.cpp
@@ -10,7 +10,7 @@ void multiplayer_process_events( World *world ) {
 void multiplayer_set_block_send_packet( NetPacket *update ) {
 }
 
-void multiplayer_set_block( int place, int block_x, int block_y, int block_z, BlockState blockState ) {
+void multiplayer_set_block( int block_x, int block_y, int block_z, BlockState blockState ) {
 }
 
 void multiplayer_update_players_position( float player_x, float player_y, float player_z, const glm::mat4 &rotation ) {
