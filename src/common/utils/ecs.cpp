b01201ed96a6c138eca01074c5e274f931360db8
Paul Repka <repkap11@gmail.com>
2024-07-29 23:58:07 -0400

    This works, but maybe i can copy
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- src/common/utils/ecs.cpp
+++ src/common/utils/ecs.cpp
@@ -5,34 +5,21 @@ struct position {
     int y;
 };
 
-struct id {
+struct Tag {
     int id;
 };
 
 int test_ecs( ) {
     pr_debug( "test_ecs" );
 
-    // we can create a entt::registry to store our entities
     entt::registry registry;
-
-    // we'll create 10 entities
-
-    const auto entity_3 = registry.create( );
     for ( std::size_t i = 0; i < 10; ++i ) {
-        entt::entity entity;
-        if ( i == 3 ) {
-            entity = entity_3;
-        } else {
-            entity = registry.create( );
-        }
+        const entt::entity entity = registry.create( );
         registry.emplace<position>( entity, i, -i );
-        registry.emplace<id>( entity, i );
+        if ( i == 3 ) {
+            registry.emplace<Tag>( entity, i );
+        }
     }
-
-    // auto view_both = registry.view<const position, const id>( );
-    // for ( auto [ entity, pos, id ] : view_both.each( ) ) {
-    //     pr_debug( "B Id:%d pos:%d %d", id.id, pos.x, pos.y );
-    // }
 
     auto group_pos = registry.group<position>( );
     auto size = group_pos.size( );
@@ -41,8 +28,13 @@ int test_ecs( ) {
     for ( std::size_t i = 0; i < size; ++i ) {
         pr_debug( "B pos:%d %d", pos[ i ].x, pos[ i ].y );
     }
-    registry.destroy( entity_3 );
 
+    auto entities_with_id = registry.view<const Tag>( );
+    for ( auto [ entity, id ] : entities_with_id.each( ) ) {
+        if ( id.id == 3 ) {
+            registry.destroy( entity );
+        }
+    }
     group_pos = registry.group<position>( );
     size = group_pos.size( );
     pos = *group_pos.storage<position>( )->raw( );
