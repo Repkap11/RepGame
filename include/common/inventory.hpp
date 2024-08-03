7abdf6c3058661f382e03304f8c5cbe6558d257d
Paul Repka <repkap11@gmail.com>
2024-08-03 00:05:54 -0400

    WIP inventory needs to be owned by repgame, not ui_overlay
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- include/common/inventory.hpp
+++ include/common/inventory.hpp
@@ -2,14 +2,20 @@
 #define HEADER_INVENTORY_H
 
 #include "common/inventory_renderer.hpp"
+#include "common/block_definitions.hpp"
+#include <map>
 
 class Inventory {
     InventoryRenderer inventory_renderer;
     InventorySlot slots[ INVENTORY_MAX_SIZE ];
+    std::map<BlockID, int> blockId_to_slot_map;
+
+    int findOpenSlot( );
 
   public:
     void init( VertexBufferLayout *ui_overlay_vbl_vertex, VertexBufferLayout *ui_overlay_vbl_instance );
     void onScreenSizeChange( int width, int height );
+    void addBlock( BlockID blockId );
     void handleInput( InputState *inputState );
     void draw( Renderer *renderer, Texture *blocksTexture, const glm::mat4 &mvp_ui, Shader *shader );
     void cleanup( );
--- src/common/inventory.cpp
+++ src/common/inventory.cpp
@@ -5,7 +5,7 @@ void Inventory::init( VertexBufferLayout *ui_overlay_vbl_vertex, VertexBufferLay
     this->inventory_renderer.init( ui_overlay_vbl_vertex, ui_overlay_vbl_instance );
     for ( int i_slot = 0; i_slot < INVENTORY_MAX_SIZE; i_slot++ ) {
         InventorySlot *slot = &this->slots[ i_slot ];
-        slot->block_id = ( BlockID )( i_slot % 4 );
+        slot->block_id = AIR;
     }
     this->inventory_renderer.render( 100, 100, this->slots );
 }
@@ -17,6 +17,28 @@ void Inventory::onScreenSizeChange( int width, int height ) {
 void Inventory::draw( Renderer *renderer, Texture *blocksTexture, const glm::mat4 &mvp_ui, Shader *shader ) {
     this->inventory_renderer.draw( renderer, blocksTexture, mvp_ui, shader );
 }
+
+int Inventory::findOpenSlot( ) {
+    for ( int i_slot = 0; i_slot < INVENTORY_MAX_SIZE; i_slot++ ) {
+        if ( this->slots[ i_slot ].block_id == AIR )
+            return i_slot;
+    }
+    return -1;
+}
+bool Inventory::addBlock( BlockID blockId ) {
+    auto it = this->blockId_to_slot_map.find( blockId );
+    if ( it == this->blockId_to_slot_map.end( ) ) {
+        // This is a new block
+        int openSlot = this->findOpenSlot( );
+        if ( openSlot == -1 ) {
+            return false;
+        }
+        this->slots[ openSlot ].block_id = blockId;
+    } else {
+        // This is an existing block, we don't support stacking yet...
+    }
+    return true;
+};
 
 void Inventory::handleInput( InputState *inputState ) {
 }
