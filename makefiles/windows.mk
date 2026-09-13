f5a10dceb50d2c07b46de365583c7621cf13a251
Paul Repka <repkap11@gmail.com>
2026-09-13 16:08:01 -0400

    Use a newer SDL to fix window build
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- makefiles/windows.mk
+++ makefiles/windows.mk
@@ -100,7 +100,7 @@ windows_build:
 	rm -rf windows_build
 	mkdir -p windows_build/glew
 	mkdir -p windows_build/sdl2
-	wget -q https://repkap11.com/files/repgame/SDL2-devel-2.0.10-mingw.tar.gz -O sdl2.tar.gz
+	wget -q https://repkap11.com/files/repgame/SDL2-devel-2.32.10-mingw.tar.gz -O sdl2.tar.gz
 	wget -q https://repkap11.com/files/repgame/glew-2.1.0-mingw-w64.zip -O glew.zip
 	bsdtar --strip-components=1 -xvzf glew.zip -C windows_build/glew
 	bsdtar --strip-components=1 -xvzf sdl2.tar.gz -C windows_build/sdl2
